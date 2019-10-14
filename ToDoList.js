cc6187dac063ebfbb4af1df23e3d22a5b9a7fd02
Nattan <kifoyinattan@gmail.com>
2019-10-14 10:13:26 +0200

    adding an item

--- ToDoList.js
+++ ToDoList.js
@@ -1,1 +1,6 @@
-/*alert('todo');*/
+function add_item (){
+    var todo_item = document.getElementById("item_input").value;
+    var list = document.getElementById("ToDoList_list_output");
+    var item = "<p id=\"item\">"+todo_item+"</p><button>Supprimez</button>";
+    list.innerHTML = list.innerHTML +item;
+}
