26bf1c16e1b00dc7c7d9d5176804ae54c42bb323
Nattan <kifoyinattan@gmail.com>
2019-10-14 10:35:27 +0200

    Add array for the task

--- ToDoList.js
+++ ToDoList.js
@@ -1,6 +1,10 @@
+var items = [];
+
 function add_item (){
     var todo_item = document.getElementById("item_input").value;
+    items.push({id: items.length+1, task:todo_item});
     var list = document.getElementById("ToDoList_list_output");
-    var item = "<p id=\"item\">"+todo_item+"</p><button>Supprimez</button>";
+    var item = "<div class=\"ToDoList_list_items\" id=\""+items[items.length-1].id+"\"><p id=\"item\">"+todo_item+"</p><button>Supprimez</button></div>";
     list.innerHTML = list.innerHTML +item;
 }
+
