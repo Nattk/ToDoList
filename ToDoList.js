5dd25d43b0c6b37864eb586ef5c0a793832bcb93
Nattan <kifoyinattan@gmail.com>
2019-10-14 11:18:11 +0200

    align items center add update function

--- ToDoList.js
+++ ToDoList.js
@@ -4,7 +4,8 @@ function add_item (){
     var todo_item = document.getElementById("item_input").value;
     items.push({id: items.length+1, task:todo_item});
     var list = document.getElementById("ToDoList_list_output");
-    var item = "<div class=\"ToDoList_list_items\" id=\"task_"+items[items.length-1].id+"\"><p id=\"item\">"+todo_item+"</p><button onclick=\"delete_item("+items[items.length-1].id+")\">Supprimez</button></div>";
+    var item = "<div class=\"ToDoList_list_items\" id=\"task_"+items[items.length-1].id+"\"><p id=\"item\">"+todo_item+"</p><button onclick=\"delete_item("+items[items.length-1].id+")\">Supprimez</button><button onclick=\"update_item("+items[items.length-1].id+")\">Modifiez</button></div>\
+    ";
     list.innerHTML = list.innerHTML +item;
 }
 
@@ -13,4 +14,9 @@ function delete_item (id){
     var list = document.getElementById("ToDoList_list_output");
     console.log(list);
     list.removeChild(todo_item);
+}
+
+function update_item (id){
+    var todo_item = document.getElementById("task_"+id);
+
 }
