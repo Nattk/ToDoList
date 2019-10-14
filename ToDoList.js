076c1133b3e78fc78bc32368620f100f5d418740
Nattan <kifoyinattan@gmail.com>
2019-10-14 10:49:19 +0200

    remove task

--- ToDoList.js
+++ ToDoList.js
@@ -4,7 +4,13 @@ function add_item (){
     var todo_item = document.getElementById("item_input").value;
     items.push({id: items.length+1, task:todo_item});
     var list = document.getElementById("ToDoList_list_output");
-    var item = "<div class=\"ToDoList_list_items\" id=\""+items[items.length-1].id+"\"><p id=\"item\">"+todo_item+"</p><button>Supprimez</button></div>";
+    var item = "<div class=\"ToDoList_list_items\" id=\"task_"+items[items.length-1].id+"\"><p id=\"item\">"+todo_item+"</p><button onclick=\"delete_item("+items[items.length-1].id+")\">Supprimez</button></div>";
     list.innerHTML = list.innerHTML +item;
 }
 
+function delete_item (id){
+    var todo_item = document.getElementById("task_"+id);
+    var list = document.getElementById("ToDoList_list_output");
+    console.log(list);
+    list.removeChild(todo_item);
+}
